e0d73bee77d6d47caa8d73e2f1aa09f7a6743b98
Matt DelSordo <mattdelsordo@gmail.com>
2019-04-15 14:36:59 -0400

    Fix video capture demo to actually capture video

--- instrument.py
+++ instrument.py
@@ -3,20 +3,31 @@
 import numpy as np
 import cv2
 
+# Open window, start video capture
+cv2.namedWindow("webcam")
 cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+# Try to open the webcam, print message if it fails
+if cap.isOpened():
+    ret, img = cap.read()
+else:
+    print("Webcam not opened (try another device id?)")
+    ret = False    
+
+while(ret):
+    # Display last frame on window
+    cv2.imshow("webcam",img)
+
+    # Capture new frame
+    ret, img = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # ESC to quit
+    if cv2.waitKey(1) == 27:
         break
 
-# When everything done, release the capture
+# When everything done, release the capture and clean up
 cap.release()
 cv2.destroyAllWindows()
