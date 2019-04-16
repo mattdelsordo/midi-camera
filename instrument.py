3a9df5b6d1443224ceaf1b2e64709beed243d766
Matt DelSordo <mattdelsordo@gmail.com>
2019-04-15 23:54:37 -0400

    Toggle squares for chunks that weren't previously occupied

--- instrument.py
+++ instrument.py
@@ -1,15 +1,31 @@
 # MIDI instrument that sends signals based on how an
 # object moves within the camera frame
-import numpy as np
 import cv2
+import numpy as np
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+# Size of the grid that the image is split into
+GRID_SIZE = 4
+last_visited = [([False]*GRID_SIZE) for i in range(GRID_SIZE)]
+active_squares = [([False]*GRID_SIZE) for i in range(GRID_SIZE)]
+chunkX = 0
+chunkY = 0
+
+# Below this intensity (0-255), don't include in the diff
+DIFF_THRESHOLD = 50
 
 # Open window, start video capture
-cv2.namedWindow("webcam")
+cv2.namedWindow("feed")
 cap = cv2.VideoCapture(0)
 
 # Try to open the webcam, print message if it fails
 if cap.isOpened():
     ret, img1 = cap.read()
+    imgX, imgY, imgChannels = img1.shape
+    chunkX = imgX / GRID_SIZE
+    chunkY = imgY / GRID_SIZE
+    print("x:", imgX, "y:", imgY, "chunk size:", chunkX, chunkY)
 else:
     print("Webcam not opened (try another device id?)")
     ret = False    
@@ -20,10 +36,28 @@ while(ret):
 
     # Calculate difference between this frame and the last frame
     diff = cv2.absdiff(img1, img2)
-    mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY) #grayscale it
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY) #grayscale it
+    # ENHANCE
+    gray[gray >= DIFF_THRESHOLD] = 255
+    gray[gray < DIFF_THRESHOLD] = 0
+    
+    # Operate on each square of the diff
+    for y in range(0, GRID_SIZE):
+        for x in range(0, GRID_SIZE):
+            startX = x * chunkX
+            startY = y * chunkY
+            # check each chunk for the presence of an object
+            if (np.count_nonzero(gray[startX:startX+chunkX, startX:startX+chunkY]) > 1):
+                # if the object wasn't here in the last frame, toggle the square
+                if not last_visited[x][y]:
+                    active_squares[x][y] = not active_squares[x][y]
+                last_visited[x][y] = True
+            else:
+                last_visited[x][y] = False
+            pp.pprint(active_squares)
 
     # Display difference between the frames on the window
-    cv2.imshow("webcam",mask)
+    cv2.imshow("feed", gray)
 
     # Set last frame to the new curent frame
     img1 = img2
